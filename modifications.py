72dc4899646dee1fbe81acf6bbac03f25df476e9
ennirajala <enni.rajala@gmail.com>
2024-08-19 14:45:57 +0300

    test

--- modifications.py
+++ modifications.py
@@ -11,12 +11,10 @@ parameters = helsinki_json["timeSeries"][0]["parameters"]
 names = ["date"]
 values = []
 values.append(date)
-#units = []
 
 for i in range(len(parameters)):
     names.append(parameters[i]["name"])
     values.append(parameters[i]["values"][0])
-    #units.append(parameters[i]["unit"])
 
 df = pd.DataFrame(np.array(values).reshape(-1,len(values)), columns=names)
    
