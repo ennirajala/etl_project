63722206af020660afc82623721ddc7cb208a13c
sinivuorensyrja <sini.vuorensyrja@brightstraining.com>
2024-08-19 11:02:03 +0300

    working API call v1

--- api.py
+++ api.py
@@ -3,51 +3,42 @@ import pandas as pd
 import json
 
 geo_locations = {
-    "tampere": (61.5, 23.8),
-    "pori": (61.5, 21.8),
-    "helsinki": (60.2, 24.9),
-    "turku": (60.5, 22.3)
+    # "tampere": (61.414710, 23.603993),
+    "helsinki": (60.317949, 24.949388),
 }
 
-API_KEY = "2892ea72bd2c25aa007cc04b653790a4"
 
-def  _create_openweather_string(lat, lon, appid):
-    url = "https://api.openweathermap.org/data/2.5/weather?lat=" + str(lat) + "&lon=" + str(lon) + "&appid=" + appid
+def  _create_openweather_string(lat, lon):
+    url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/" + str(lon) + "/lat/" + str(lat) + "/data.json"
     return url
 
-def _get_openweather(lat, lon, appid):
+def _get_openweather(lat, lon):
 
-    r = requests.get(_create_openweather_string(lat, lon, appid))
+    r = requests.get(_create_openweather_string(lat, lon))
 
     print("url:", r.url)
     print("http code:", r.status_code)
 
     json_data = r.json() 
 
+    return json_data
+
+
+def _json_to_csv(json_data):
+
     weather_data = {
-        "weather": json_data["weather"][0],
-        "main": json_data["main"],
-        "visibility": json_data["visibility"],
-        "wind": json_data["wind"],
-        "clouds": json_data["clouds"]
+        "date": json_data["timeSeries"][0]
+        # "percipitation": json_data["timeSeries"][0][0],
+        # "visibility": json_data["visibility"],
+        # "wind": json_data["wind"],
+        # "clouds": json_data["clouds"]
     }
 
     weather_data = pd.json_normalize(weather_data) 
 
     df = pd.DataFrame(weather_data)
-    df.to_csv("/home/ennirajala/my_project/airflow/dags/" + str(lon) + ".csv", index=False)
+    df.to_csv("/home/sinivuor/my_project/wip/test2.csv", index=False)
 
-    return json_data
 
-def _write_weather_log(lat, lon, appid):
-    json_list = []
-
-    for city in geo_locations:
-        (lat, lon) = geo_locations[city]
-
-        json_list.append(_get_openweather(lat, lon, appid))
-    
-    with open("weather_log.json", "w") as final:
-        json.dump(json_list, final)
-
-_write_weather_log(61.5, 23.8, API_KEY)
+test1 = _get_openweather(60.317949, 24.949388)
+_json_to_csv(test1)
