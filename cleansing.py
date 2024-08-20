637b0853e13cf45762280b3a68de7558169446a9
ennirajala <enni.rajala@gmail.com>
2024-08-20 10:30:32 +0300

    continued clenasing

--- cleansing.py
+++ cleansing.py
@@ -1,81 +1,101 @@
 import pandas as pd
 
+PATH_ENNI = "/home/ennirajala/enni-sini/"
+PATH_SINI = "/home/sinivuor/enni-sini/etl_project/"
+
+# Read csv files as a Pandas data frame
 fmi_df = pd.read_csv("fmi.csv")
 smhi_df = pd.read_csv("smhi.csv")
 
-# Clean the fmi dataframe
-dropped_fmi = ["Maximum temperature [°C]",
-               "Minimum temperature [°C]",
-               "Maximum wind speed [m/s]",
-               "Maximum gust speed [m/s]",
-               ]
-fmi_df = fmi_df.drop(dropped_fmi, axis=1)
+# Clenses fmi Pandas data frame
+def _cleanse_fmi_data(fmi_df):
+    # Drop columns that exist only in this fmi data
+    dropped_fmi = ["Maximum temperature [°C]",
+                "Minimum temperature [°C]",
+                "Maximum wind speed [m/s]",
+                "Maximum gust speed [m/s]",
+                ]
+    fmi_df = fmi_df.drop(dropped_fmi, axis=1)
 
-fmi_df['Date'] = fmi_df['Year'].astype(str) +"-0"+ fmi_df['Month'].astype(str) +"-"+ fmi_df['Day'].astype(str)
-fmi_df = fmi_df.drop(['Year', 'Month', 'Day'], axis=1)
+    # Merge Year, Month and Day as a new Date column
+    fmi_df['Date'] = fmi_df['Year'].astype(str) +"-0"+ fmi_df['Month'].astype(str) +"-"+ fmi_df['Day'].astype(str)
+    fmi_df = fmi_df.drop(['Year', 'Month', 'Day'], axis=1)
 
-fmi_df = fmi_df.reindex(columns=["Date",
-                                 "Time [Local time]",
-                                 "Observation station",
-                                 "Average temperature [°C]",
-                                 "Average relative humidity [%]",
-                                 "Wind speed [m/s]",
-                                 "Average wind direction [°]",
-                                 "Precipitation [mm]",
-                                 "Average air pressure [hPa]"])
+    # Rearrange the columns to be in spesific order
+    fmi_df = fmi_df.reindex(columns=["Date",
+                                    "Time [Local time]",
+                                    "Observation station",
+                                    "Average temperature [°C]",
+                                    "Average relative humidity [%]",
+                                    "Wind speed [m/s]",
+                                    "Average wind direction [°]",
+                                    "Precipitation [mm]",
+                                    "Average air pressure [hPa]"])
 
-fmi_df.rename(columns={"Time [Local time]": 'Time',
-                   "Observation station": 'Location',
-                   "Average temperature [°C]": "Temperature [°C]",
-                   "Average relative humidity [%]":"Relative humidity [%]",
-                   "Average wind direction [°]": "Wind direction [°]",
-                   "Average air pressure [hPa]": "Air pressure [hPa]"
-                   }, inplace=True)
+    # Rename the columns to have spesicif names
+    fmi_df.rename(columns={"Time [Local time]": 'Time',
+                    "Observation station": 'Location',
+                    "Average temperature [°C]": "Temperature [°C]",
+                    "Average relative humidity [%]":"Relative humidity [%]",
+                    "Average wind direction [°]": "Wind direction [°]",
+                    "Average air pressure [hPa]": "Air pressure [hPa]"
+                    }, inplace=True)
 
-fmi_df.to_csv("/home/ennirajala/enni-sini/fmi_cleaned.csv", index=False)
+    # Save clensed data as csv file
+    fmi_df.to_csv(PATH_ENNI + "fmi_cleaned.csv", index=False)
 
-# Clean the smhi dataframe
-dropped_smhi = ["vis",
-                "tstm",
-                "tcc_mean",
-                "lcc_mean",
-                "mcc_mean",
-                "hcc_mean",
-                "gust",
-                "pmin",
-                "pmax",
-                "spp",
-                "pcat",
-                "Wsymb2",
-                "pmedian"]
-smhi_df = smhi_df.drop(dropped_smhi, axis=1)
+# Clenses smhi Pandas data frame
+def _cleanse_smhi_data(smhi_df):
+    # Drop columns that exist only in this smhi data
+    dropped_smhi = ["vis",
+                    "tstm",
+                    "tcc_mean",
+                    "lcc_mean",
+                    "mcc_mean",
+                    "hcc_mean",
+                    "gust",
+                    "pmin",
+                    "pmax",
+                    "spp",
+                    "pcat",
+                    "Wsymb2",
+                    "pmedian"]
+    smhi_df = smhi_df.drop(dropped_smhi, axis=1)
 
-smhi_df[['Date', 'Time']] = smhi_df['date'].str.split('T', n=1, expand=True)
-smhi_df = smhi_df.drop(["date"], axis=1)
+    # Separate date column to be two new columns Date and Time
+    smhi_df[['Date', 'Time']] = smhi_df['date'].str.split('T', n=1, expand=True)
+    smhi_df = smhi_df.drop(["date"], axis=1)
 
-smhi_df = smhi_df.reindex(columns=["Date",
-                                 "Time",
-                                 "location",
-                                 "t",
-                                 "r",
-                                 "ws",
-                                 "wd",
-                                 "pmean",
-                                 "msl"])
+    # Rearrange the columns to be in spesific order
+    smhi_df = smhi_df.reindex(columns=["Date",
+                                    "Time",
+                                    "location",
+                                    "t",
+                                    "r",
+                                    "ws",
+                                    "wd",
+                                    "pmean",
+                                    "msl"])
+    
+    # Rename the columns to have spesicif names
+    smhi_df.rename(columns={"location": 'Location',
+                    "t": "Temperature [°C]",
+                    "r":"Relative humidity [%]",
+                    "ws": "Wind speed [m/s]",
+                    "wd": "Wind direction [°]",
+                    "pmean": "Precipitation [mm]",
+                    "msl": "Air pressure [hPa]"
+                    }, inplace=True)
 
-smhi_df.rename(columns={"location": 'Location',
-                   "t": "Temperature [°C]",
-                   "r":"Relative humidity [%]",
-                   "ws": "Wind speed [m/s]",
-                   "wd": "Wind direction [°]",
-                   "pmean": "Precipitation [mm]",
-                   "msl": "Air pressure [hPa]"
-                   }, inplace=True)
+    # Cleanse columns values to match fmi's data
+    smhi_df.loc[0, 'Time'] = "10:00"
+    smhi_df.loc[1, 'Time'] = "10:00"
 
-smhi_df.loc[0, 'Time'] = "10:00"
-smhi_df.loc[1, 'Time'] = "10:00"
+    smhi_df.loc[0, 'Location'] = "Vantaa Helsinki-Vantaa airport"
+    smhi_df.loc[1, 'Location'] = "Pirkkala Tampere-Pirkkala airport"
 
-smhi_df.loc[0, 'Location'] = "Vantaa Helsinki-Vantaa airport"
-smhi_df.loc[1, 'Location'] = "Pirkkala Tampere-Pirkkala airport"
+    # Save clensed data as csv file
+    smhi_df.to_csv(PATH_ENNI + "smhi_cleaned.csv", index=False)
 
-smhi_df.to_csv("/home/ennirajala/enni-sini/smhi_cleaned.csv", index=False)
+_cleanse_fmi_data(fmi_df)
+_cleanse_smhi_data(smhi_df)
--- postgres.py
+++ postgres.py
@@ -1,28 +1,27 @@
-# The code presums a postgres connection on localhost
-
 import os
 import pandas as pd
-import psycopg2 as ps  # postgres sql
-
-from sqlalchemy import create_engine  # sql driver
-
+import psycopg2 as ps
+from sqlalchemy import create_engine
 
 CURR_DIR_PATH = os.path.dirname(os.path.realpath(__file__))
 
-def postgres_creator():  # sqlalchemy requires callback-function for connections
+# Creates a connection to postgres server
+def postgres_creator():
   return ps.connect(
-        dbname="enni_sini",  # name of schema
+        dbname="enni_sini",
         user="enni_sini",
         password="enni_sini",
         host="localhost",
         port="5432"
   )
 
+# Create postgres engine
 postgres_engine = create_engine(
-    url="postgresql+psycopg2://localhost",  # driver identification + dbms api
-    creator=postgres_creator  # connection details
+    url="postgresql+psycopg2://localhost",
+    creator=postgres_creator
 )
 
+# Create Pandas dataframes from cleansed data csvs
 smhi_data = pd.read_csv(
     CURR_DIR_PATH + "/smhi_cleaned.csv",
     sep=",",
@@ -33,7 +32,6 @@ fmi_data = pd.read_csv(
     sep=",",
 )
 
-
-# Write to postgres
+# Create two postgres tables according to data source
 smhi_data.to_sql(name="smhi_data", con=postgres_engine, if_exists="replace", index=False)
 fmi_data.to_sql(name="fmi_data", con=postgres_engine, if_exists="replace", index=False)
