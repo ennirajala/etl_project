292b6afdb81cbc00c41f7f0ceb4e98e590b1a9f7
ennirajala <enni.rajala@gmail.com>
2024-08-20 10:03:02 +0300

    added comments and renamed files

--- api.py
+++ api.py
@@ -2,26 +2,33 @@ import requests, os, configparser
 import pandas as pd
 import json
 
+# Coordinates of the locations
+HKI_COORDS = [60.317949, 24.949388]
+TRE_COORDS= [61.414710, 23.603993]
+
+# Json files names
+hki_filename = "helsinki"
+tre_filename = "tampere"
+
+# Get weather data with API as a JSON format
 def  _get_weather(lat, lon):
     url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/" + str(lon) + "/lat/" + str(lat) + "/data.json"
 
     r = requests.get(url)
 
-    # print("url:", r.url)
-    # print("http code:", r.status_code)
-
     json_data = r.json()
 
     return json_data
 
+# Save weather data as JSON file
 def _save_weather(json_data, filename):
    
    with open(filename + ".json", "w") as outfile:
     json.dump(json_data, outfile) 
 
-helsinki = _get_weather(60.317949, 24.949388)
-_save_weather(helsinki, "helsinki")
+helsinki = _get_weather(HKI_COORDS[0], HKI_COORDS[1])
+_save_weather(helsinki, hki_filename)
 
-tampere = _get_weather(61.414710, 23.603993)
-_save_weather(tampere, "tampere")
+tampere = _get_weather(TRE_COORDS[0], TRE_COORDS[1])
+_save_weather(tampere, tre_filename)
 
---
+++ harmonizing.py
@@ -0,0 +1,71 @@
+import pandas as pd
+import json
+import numpy as np
+
+PATH_ENNI = "/home/ennirajala/enni-sini/"
+PATH_SINI = "/home/sinivuor/enni-sini/etl_project/"
+
+def _harmonize_smhi_data():
+    # Reads json files
+    with open('helsinki.json', 'r') as f:
+        helsinki_json = json.load(f)
+
+    with open('tampere.json', 'r') as f:
+        tampere_json = json.load(f)
+
+    # Slice only the parts we need from json data
+    date = helsinki_json["timeSeries"][0]["validTime"]
+    parameters_hki = helsinki_json["timeSeries"][0]["parameters"]
+    parameters_tre = tampere_json["timeSeries"][0]["parameters"]
+
+    # Create lists for Pandas data frames columns(names) and rows(values)
+    names = ["date", "location"]
+
+    values_hki = []
+    values_hki.append(date)
+    values_hki.append("Helsinki")
+
+    values_tre = []
+    values_tre.append(date)
+    values_tre.append("Tampere")
+    
+    # Iterate through parameters lists and append values to columns and rows lists
+    for i in range(len(parameters_hki)):
+        names.append(parameters_hki[i]["name"])
+        values_hki.append(parameters_hki[i]["values"][0])
+        values_tre.append(parameters_tre[i]["values"][0])
+
+    # Create Pandas data frame and reindex it
+    df = pd.DataFrame(np.array(values_hki).reshape(-1,len(values_hki)), columns=names)
+
+    df.loc[-1] = values_tre
+    df.index = df.index + 1
+
+    # Create csv file for harmonized data
+    df.to_csv(PATH_ENNI + "smhi.csv", index=False)
+ 
+def _harmonize_fmi_data():
+    # Create Pandas data frames from fmi's csv files
+    fmi_hki_df = pd.read_csv("helsinki_met.csv")
+    fmi_tre_df = pd.read_csv("tampere_met.csv")
+
+    # Slice only the rows we need from Pandas data frame
+    clean_hki_df = fmi_hki_df[fmi_hki_df['Time [Local time]'] == "10:00"]
+    clean_tre_df = fmi_tre_df[fmi_tre_df['Time [Local time]'] == "10:00"]
+
+    # Merge data from two locations into same Pandas data frame
+    fmi_df = pd.concat([clean_hki_df, clean_tre_df], axis=0)
+
+    # Create csv file for harmonized data
+    fmi_df.to_csv(PATH_ENNI + "fmi.csv", index=False)
+
+_harmonize_smhi_data()
+_harmonize_fmi_data()
+
+
+
+
+
+
+
+
